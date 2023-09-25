8f8881b0c6a51d66d04fd976910499feb415c10d
Larissa <laripedrosasilva@gmail.com>
2023-09-25 14:38:06 -0300

    Adicionando clique aos botoes e metodo clear

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,32 @@
 const createCalculator = () => {
-    return{
+    return {
+        display: document.querySelector('.display'),
+        btnClear: document.querySelector('.btn-clear'),
 
+        start() {
+            this.clickbuttons();
+        },
+
+        clickbuttons() {
+            document.addEventListener('click', e => {
+                const el = e.target;
+
+                if (el.classList.contains('btn-num')) {
+                    this.btnForDisplay(el.innerText);
+                }
+            });
+        },
+
+        btnForDisplay(valor) {
+            this.display.value += valor;
+        },
+
+        clearDisplay(){
+            this.display.value = '';
+        }
     };
 }
 
 
 const calculator = createCalculator();
+calculator.start();
